2cad26c6fb5fae66ad9bb0a103a20f3cd4b372e9
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-08-15 01:34:09 -0400

    changed cube with moon but app is crushing

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,19 +22,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        let sphere = SCNSphere(radius: 0.2)
         
         let material = SCNMaterial()
         
-        material.diffuse.contents = UIColor.red
-        
-        cube.materials = [material]
+        if let image = UIImage(named: "art.scnassets/moon.jpg") {
+            material.diffuse.contents = image
+            sphere.materials = [material]
+        } else {
+            print("Error: The image 'moon.jpg' was not found in the art.scnassets folder.")
+        }
         
         let node = SCNNode()
         
         node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
         
-        node.geometry = cube
+        node.geometry = sphere
         
         sceneView.scene.rootNode.addChildNode(node)
         
